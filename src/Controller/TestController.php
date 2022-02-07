0ebb117c0681d0554bc3f7ae3df93939d3c782bd
Bounar64 <rd.bounar@gmail.com>
2022-02-07 19:36:50 +0100

    first page

---
+++ src/Controller/TestController.php
@@ -0,0 +1,24 @@
+<?php  
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class TestController {
+
+    public function index() {
+        
+        dd("ca fonctionne");
+    }
+
+    /**
+     * @Route("/test/{age<\d+>?0}", name="test", methods={"GET", "POST"})
+     */
+    public function test(Request $request, $age) {
+
+        return new Response("Vous avez $age ans");
+    }
+}
