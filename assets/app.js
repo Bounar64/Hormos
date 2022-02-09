3c8a2b23728a5fc7f4c6150a719eeeffee07a5d8
Bounar64 <rd.bounar@gmail.com>
2022-02-09 17:21:28 +0100

    Ajout d'une vidéo

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,26 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+
+//** Au survole bouton play un popover indique "Lire la vidéo" avec un délai avant l'affichage */
+const play = document.getElementById('play');
+document.getElementById('popover').style.display = 'none';
+
+function afficher() {
+    document.getElementById('popover').style.display = 'block';   
+}
+
+play.addEventListener('mouseover', function() { // délai de 1 seconde pour afficher si pas de click
+    setTimeout(afficher, 1000);
+});  
+
+play.addEventListener('mouseout', function() {
+    document.getElementById('popover').style.display = 'none';
+});
+
+//** Au click du lecteur lancement de la vidéo */
+play.addEventListener('click', function() {
+    document.getElementById('miniature').style.display = 'none';
+    document.getElementById('video').play();
+});
