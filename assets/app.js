deb040d11c6843a416ab635c2252bb07b57352c4
Bounar64 <rd.bounar@gmail.com>
2022-02-12 22:19:04 +0100

    header mobile

--- assets/app.js
+++ assets/app.js
@@ -59,19 +59,56 @@ window.addEventListener('scroll', function() {
     var logoHeader = document.getElementById('logo-header');
     var header = document.querySelector('header');
     var headerMobile = document.getElementById('header-mobile');
+    var blockDropdown = document.getElementById('search-dropdown-background');
 
-    blockHead.classList.toggle('none', window.scrollY > 0); // Mini entête du header disparait
-    logoHeader.classList.toggle('up', window.scrollY > 0);
-    header.classList.toggle('Shadow', window.scrollY > 0); // Ombre dessous de la navbar s'affiche
-    headerMobile.classList.toggle('Shadow', window.scrollY > 0); // Ombre dessous de la navbar-mobile s'affiche
+    blockHead.classList.toggle('none', window.pageYOffset > 0); // Mini entête du header au top 0 disparaît
+    logoHeader.classList.toggle('up', window.pageYOffset > 0);
+    header.classList.toggle('Shadow', window.pageYOffset > 0); // Ombre dessous de la navbar s'affiche
+    headerMobile.classList.toggle('Shadow', window.pageYOffset > 0); // Ombre dessous de la navbar-mobile s'affiche
+    headerMobile.classList.toggle('Shadow', window.pageYOffset > 0); // Ombre dessous de la navbar-mobile s'affiche
+    blockDropdown.classList.toggle('up', window.pageYOffset > 0); // Fermeture jour entre input search et block suggestions
 });
 
-
+//** Fonction pour la bar de recherche qui s'ouvrve au click et se ferme au click de la croix */
 document.getElementById('input-search').onclick = function() {
     SearchGrowfunction();
 };
 
+document.getElementById('cross').onclick = function() {
+    SearchClosefunction();
+};
+
+
 function SearchGrowfunction() {
-    document.getElementById('search').classList.add('grow');   
+    document.getElementById('input-search').classList.add('grow');   
+    document.getElementById('cross').classList.add('show');
+    document.getElementById('search').classList.remove('close');   
     document.getElementById('logo-header').classList.add('d-none');   
+    document.getElementById('search-dropdown-background').classList.add('dropdown'); // Affiche bakcground dropdown en plein écran
+    document.getElementById('dropdown-list').classList.add('dropright'); // Drop coté droite des textes des suggestions de la bar de recherche
 }
+
+function SearchClosefunction() {
+    document.getElementById('search').classList.remove('grow');   
+    document.getElementById('cross').classList.remove('show');   
+    document.getElementById('search').classList.add('close');
+    document.getElementById('logo-header').classList.remove('d-none');
+    document.getElementById('search-dropdown-background').classList.remove('dropdown');     
+    document.getElementById('dropdown-list').classList.remove('dropright');     
+}
+
+
+
+document.getElementById('icon-search').onclick = function() {
+    SearchGrowMobilefunction();
+};
+
+function SearchGrowMobilefunction() {
+    document.getElementById('search-dropdown-background').classList.add('dropdown');   
+}
+
+
+
+// window.onresize = function() {
+//     alert('Redimensionnement de la fenêtre')
+// }
